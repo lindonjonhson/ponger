d34b2e8174ad309667d8accfa14b9c127136944b
Lindon Morais <lindon.jonhsonf@gmail.com>
2025-01-26 11:10:00 +0000

    Added paddles and ball

--- main.lua
+++ main.lua
@@ -8,6 +8,8 @@ WINDOW_HEIGHT = 720
 VIRTUAL_WIDTH = 432     -- declaring variables for virtual window
 VIRTUAL_HEIGHT = 243
 
+PADDLE_SPEED = 200
+
 function love.load() -- Starts the game
 
     love.graphics.setDefaultFilter('nearest', 'nearest') -- using a text filter
@@ -17,13 +19,6 @@ function love.load() -- Starts the game
         resizable = false,
         vsync = true
     })
-    
-    --[[love.window.setMode(WINDOW_WIDTH, WINDOW_HEIGHT,
-    {
-        fullscreen = false,
-        resizable = false,
-        vsync = true
-    })]]--
 end
 
 function love.keypressed(key) -- If escape is hit, quit the game
@@ -38,14 +33,24 @@ function love.draw()
 
     push:apply('start') -- Push will start rendering the virtual resolution
 
+    love.graphics.clear(0/255, 143/255, 17/255, 255/255) -- setting screen color to green
 
 
+    -- title
     love.graphics.printf(
-        'Part 1 of Pong!',
-        0,
-        VIRTUAL_HEIGHT/2-6,
-        VIRTUAL_WIDTH,
-        'center'
+        'Part 1 of Pong!', 0, 10, VIRTUAL_WIDTH, 'center'
     )
+
+    -- creating the objects
+
+    -- first paddle - left side
+    love.graphics.rectangle('fill', 10, 30, 5, 20)
+
+    -- second paddle - right side
+    love.graphics.rectangle('fill', VIRTUAL_WIDTH - 10, VIRTUAL_HEIGHT - 50, 5, 20)
+
+    -- ball
+    love.graphics.rectangle('fill', VIRTUAL_WIDTH/2 - 2, VIRTUAL_HEIGHT/2 - 2, 4, 4)
+
     push:apply('end')
 end
